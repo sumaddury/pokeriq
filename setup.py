aeec68bc7db7f2ef17482583c6fbd19c1194d7e0
Sucheer Maddury <80368850+sumaddury@users.noreply.github.com>
2025-01-13 17:50:24 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pokeriq', 
-    version='0.1.0',      
+    version='0.2.0',      
     packages=find_packages(),  
     install_requires=[
         'scipy>=1.2.0'
