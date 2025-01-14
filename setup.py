bae2fa628412cffaa974c8b6d44dd2d1bd8baa96
Sucheer Maddury <80368850+sumaddury@users.noreply.github.com>
2025-01-13 17:50:08 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     name='pokeriq', 
     version='0.1.0',      
     packages=find_packages(),  
-    install_requires=[],     
+    install_requires=[
+        'scipy>=1.2.0'
+    ],     
     description='A Micro-Library for Holdem Simulation',  
     python_requires='>=3.7',  
     url='https://github.com/sumaddury/pokeriq',
