19e174a2c1aa0548932f916994f3d75533670299
Sucheer Maddury <80368850+sumaddury@users.noreply.github.com>
2025-01-13 17:56:25 -0800

    Update __init__.py

--- pokeriq/__init__.py
+++ pokeriq/__init__.py
@@ -9,6 +9,6 @@ from .equity_tools import EquitySolver
 
 __all__ = ['Card', 'Deck', 'Hand', 'Player', 'Simulation', 'EquitySolver']
 __title__ = 'pokeriq'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Sucheer Maddury <sm2939@cornell.edu>'
 __license__ = 'Apache 2.0'
